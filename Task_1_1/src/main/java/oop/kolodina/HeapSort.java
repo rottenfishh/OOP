eae21415b1043e84660c5f9d66ab8c316d1f3702
Alina <alinakolodina591@gmail.com>
2024-09-05 21:13:16 +0700

    task-1-1

---
+++ Task_1_1/src/main/java/oop/kolodina/HeapSort.java
@@ -0,0 +1,49 @@
+package oop.kolodina;
+
+import java.util.Scanner;
+
+public class HeapSort {
+    /*
+     *method to sift down non-leaf element until it reaches it's correct position in max-heap
+     *@param arr - input array
+     *@param n - number of elements in array
+     *@param idx - index of the element we run heapify from
+     */
+    public void heapify(int[] arr, int n, int idx) {
+        int max = idx;
+        int left = 2 * idx + 1;
+        int right = 2 * idx + 2;
+        if (left < n && arr[left] > arr[max])
+            max = left;
+        if (right < n && arr[right] > arr[max])
+            max = right;
+        if (max != idx) {
+            int temp = arr[max];
+            arr[max] = arr[idx];
+            arr[idx] = temp;
+            heapify(arr, n, max);
+        }
+    }
+    /*
+     * building the max-heap and retrieving sorted array
+     * @param arr - input array to be sorted
+     * @param n - number of elements in array
+     */
+    public void heapsort(int[] arr, int n) {
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(arr, n, i);
+        }
+        for (int j = n - 1; j >= 0; j--) {
+            int temp = arr[0];
+            arr[0] = arr[j];
+            arr[j] = temp;
+            heapify(arr, j, 0);
+        }
+    }
+    /*
+    main method
+    @param args - standart arguments
+     */
+    public static void main(String[] args){
+    }
+}
