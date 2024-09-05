419f912754a765792a387f49a53117fde149e4bd
rottenfishh <146651269+rottenfishh@users.noreply.github.com>
2024-09-05 20:04:00 +0700

    Delete Task_1_1 directory

--- Task_1_1/build.sh
+++
@@ -1,7 +0,0 @@
-javadoc -d documentation/ -sourcepath src/main/java -subpackages oop.kolodina
-
-javac -d class src/main/java/oop/kolodina/HeapSort.java
-
-jar cfe HeapSort.jar oop.kolodina.HeapSort -C class .
-
-java -jar HeapSort.jar
