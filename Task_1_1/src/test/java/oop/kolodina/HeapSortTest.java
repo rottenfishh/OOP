8e4b54a4df9c381d314a5e16b2ab78d1e094a994
Alina <alinakolodina591@gmail.com>
2024-09-06 17:55:25 +0700

    change codestyle

--- Task_1_1/src/test/java/oop/kolodina/HeapSortTest.java
+++ Task_1_1/src/test/java/oop/kolodina/HeapSortTest.java
@@ -1,37 +1,41 @@
 package oop.kolodina;
 
-import static java.lang.Integer.MIN_VALUE;
 import static java.lang.Integer.MAX_VALUE;
+import static java.lang.Integer.MIN_VALUE;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 import java.util.Arrays;
 import java.util.Random;
 import org.junit.jupiter.api.Test;
 
+/**
+ *tests for heapsort.
+ */
 public class HeapSortTest {
     /**
-    testing with random arrays
+    *testing with random arrays.
      */
     @Test
     public void testArraysWithRandom() {
-        for (int i=0; i<10000; i++) {
+        for (int i = 0; i < 10000; i++) {
             Random random = new Random();
-            int RandomLength = random.nextInt(100000);
-            int[] array1 = random.ints(RandomLength, 10, 100000).toArray();
-            int[] array2 = new int[RandomLength];
-            System.arraycopy(array1, 0, array2, 0, RandomLength);
-            HeapSort.heapsort(array1, RandomLength);
+            int randomLength = random.nextInt(100000);
+            int[] array1 = random.ints(randomLength, 10, 100000).toArray();
+            int[] array2 = new int[randomLength];
+            System.arraycopy(array1, 0, array2, 0, randomLength);
+            HeapSort.heapsort(array1, randomLength);
             Arrays.sort(array2);
             assertArrayEquals(array1, array2);
         }
     }
 
     /**
-    testing edge cases with max and min values
+    *testing edge cases with max and min values.
      */
     @Test
     public void testEdges() {
-        int[] array1 = {MIN_VALUE, MIN_VALUE, MAX_VALUE, MAX_VALUE, -1000000, 321534, 0, -132456876, 777};
+        int[] array1 = {MIN_VALUE, MIN_VALUE, MAX_VALUE, MAX_VALUE,
+                -1000000, 321534, 0, -132456876, 777};
         int[] array2 = new int[array1.length];
         System.arraycopy(array1, 0, array2, 0, array1.length);
         HeapSort object = new HeapSort();
@@ -41,7 +45,7 @@ public class HeapSortTest {
     }
 
     /**
-    testing array with equal elements
+    *testing array with equal elements.
      */
     @Test
     public void checkEquals() {
@@ -50,18 +54,18 @@ public class HeapSortTest {
         System.arraycopy(array1, 0, array2, 0, array1.length);
         HeapSort.heapsort(array1, array1.length);
         Arrays.sort(array2);
-        assertArrayEquals(array1,array2);
+        assertArrayEquals(array1, array2);
     }
 
     /**
-    testing array with one element
+    *testing array with one element.
      */
     @Test
     public void checkOneElem() {
         int[] array1 = {100};
         int[] array2 = {100};
-        HeapSort.heapsort(array1,array1.length);
+        HeapSort.heapsort(array1, array1.length);
         Arrays.sort(array2);
-        assertArrayEquals(array1,array2);
+        assertArrayEquals(array1, array2);
     }
 }
