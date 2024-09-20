59a427af895df46c3cf1707018bac7ec1b67e58c
Alina Kolodina <a.kolodina@g.nsu.ru>
2024-09-20 23:54:19 +0700

    fix codestyle

--- Task_1_1_2/src/test/java/oop/kolodina/CardTest.java
+++ Task_1_1_2/src/test/java/oop/kolodina/CardTest.java
@@ -1,11 +1,12 @@
 package oop.kolodina;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 
 /**
  * tests for Card class.
