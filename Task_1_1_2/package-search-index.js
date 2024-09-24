7a5c06198df71f2351e0f140b2268f89fad5bf3b
rottenfishh <rottenfishh@users.noreply.github.com>
2024-09-24 07:46:39 +0000

    Deploying to gh-pages from @ rottenfishh/OOP@407642aff9ed2fc372b950ee850bd5591325256e 🚀

--- Task_1_1_2/package-search-index.js
+++ Task_1_1_2/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"oop.kolodina"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"ru.nsu.kolodina.blackjack"}]
