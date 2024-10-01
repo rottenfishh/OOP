98a7ec8997ec392c51e86880aefdb5181ddf4c55
Alina Kolodina <a.kolodina@g.nsu.ru>
2024-10-01 22:02:29 +0700

    main using parsing

--- Task_1_1_3/src/main/java/ru/nsu/kolodina/expressions/Main.java
+++ Task_1_1_3/src/main/java/ru/nsu/kolodina/expressions/Main.java
@@ -1,11 +1,20 @@
 package ru.nsu.kolodina.expressions;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
+
+import static ru.nsu.kolodina.expressions.Parser.polishNotation;
+import static ru.nsu.kolodina.expressions.Parser.returnExpression;
 
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String inputString = scanner.nextLine();
+        Expression expr = returnExpression(inputString);
+        expr.printExpression();
         Map<String, Double> map = new HashMap<>();
         map.put("x", 10.0);
         Expression e = new Add(new Number(3), new Mul(new Number(2),
