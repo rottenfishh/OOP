d7a39d832982d929c9d723ab56b3d1c520695ff9
Alina Kolodina <a.kolodina@g.nsu.ru>
2024-10-03 12:26:28 +0700

    fix codestyle

--- Task_1_1_3/src/test/java/ru/nsu/kolodina/expressions/SimplifyTest.java
+++ Task_1_1_3/src/test/java/ru/nsu/kolodina/expressions/SimplifyTest.java
@@ -3,8 +3,11 @@ package ru.nsu.kolodina.expressions;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static ru.nsu.kolodina.expressions.Parser.returnExpression;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+/**
+ * testing simplify method.
+ */
 public class SimplifyTest {
     Expression expr;
     String exprString;
@@ -12,6 +15,8 @@ public class SimplifyTest {
     Expression simpleExpr;
     /**
      * simple test.
+     * simplify should evaluate expression
+     * because it has no variables
      */
     @Test
     public void testSimple() {
@@ -22,15 +27,23 @@ public class SimplifyTest {
         assertEquals(excepted, simpleExpr.convertToString());
     }
 
+    /**
+     * testing multiplication with zero.
+     * expression should be evaluated to zero constant
+     */
     @Test
     public void testMulZero() {
-        exprString = "(3+243-4/2) * 0";
+        exprString = "(3+243-4/x) * 0";
         expr = returnExpression(exprString);
         simpleExpr = expr.simplify();
         excepted ="0.0";
         assertEquals(excepted, simpleExpr.convertToString());
     }
 
+    /**
+     * testing multiplication with one.
+     * expression should be changed to its other multiplier
+     */
     @Test
     public void testMulOne() {
         exprString = "(34 - 31 * x) * 1";
@@ -40,6 +53,10 @@ public class SimplifyTest {
         assertEquals(excepted, simpleExpr.convertToString());
     }
 
+    /**
+     * testing sub with equal expressions.
+     * expression should change to zero constant
+     */
     @Test
     public void subEqualExpr() {
         exprString = "(243 * 21 + 2) - (243 * 21 +2)";
