4b29a494603a7a6617297648f5639b14536a3022
Alina Kolodina <a.kolodina@g.nsu.ru>
2024-10-07 23:42:02 +0700

    fix codestyle

--- Task_1_1_3/src/test/java/ru/nsu/kolodina/expressions/EqualsTest.java
+++ Task_1_1_3/src/test/java/ru/nsu/kolodina/expressions/EqualsTest.java
@@ -1,7 +1,7 @@
 package ru.nsu.kolodina.expressions;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
