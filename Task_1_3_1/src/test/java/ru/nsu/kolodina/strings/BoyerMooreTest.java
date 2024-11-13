f67e928357a932f7b5a7691414a200bbaaae4059
Alina Kolodina <a.kolodina@g.nsu.ru>
2024-11-13 22:28:11 +0700

    lmao

---
+++ Task_1_3_1/src/test/java/ru/nsu/kolodina/strings/BoyerMooreTest.java
@@ -0,0 +1,133 @@
+package ru.nsu.kolodina.strings;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class BoyerMooreTest {
+    BoyerMoore boyerMoore;
+
+    @BeforeEach
+    void setUp() {
+        boyerMoore = new BoyerMoore();
+    }
+
+    @Test
+    public void testSmall() throws IOException {
+        File newFile = new File("file1.txt");
+        OutputStreamWriter fileWriter = new OutputStreamWriter(new FileOutputStream(newFile), StandardCharsets.UTF_8);
+        String string = new String("абракадабра".getBytes(), StandardCharsets.UTF_8);
+        fileWriter.write(string);
+        fileWriter.close();
+        List<Integer> resultBoyerMoore = boyerMoore.findInFile("file1.txt", "бра");
+        List<Integer> excepted = new ArrayList<>();
+        excepted.add(1);
+        excepted.add(8);
+        assertEquals(excepted, resultBoyerMoore);
+        newFile.delete();
+    }
+
+    @Test
+    public void testBig() throws IOException {
+        File newFile = new File("file2.txt");
+        OutputStreamWriter fileWriter = new OutputStreamWriter(new FileOutputStream(newFile), StandardCharsets.UTF_8);
+        char[] chunk = new char[20000];
+        Arrays.fill(chunk, 'a');
+        fileWriter.write(chunk);
+        fileWriter.write("hello");
+        Arrays.fill(chunk, 'l');
+        fileWriter.write(chunk);
+        fileWriter.close();
+        String pattern = "hello";
+        List<Integer> resultBoyerMoore = boyerMoore.findInFile("file2.txt", pattern);
+        List<Integer> excepted = new ArrayList<>();
+        excepted.add(20000);
+        assertEquals(excepted, resultBoyerMoore);
+        newFile.delete();
+    }
+
+    @Test
+    void testReallyBig() throws IOException {
+        int maxSize = 100000;
+        File newFile = new File("file3.txt");
+        OutputStreamWriter fileWriter = new OutputStreamWriter(new FileOutputStream(newFile), StandardCharsets.UTF_8);
+
+        char[] chunk = new char[maxSize];
+        Arrays.fill(chunk, 'h');
+        fileWriter.write(chunk);
+        fileWriter.write("hello");
+        Arrays.fill(chunk, 'l');
+        fileWriter.write(chunk);
+        fileWriter.close();
+        String pattern = "hello";
+        List<Integer> resultBoyerMoore = boyerMoore.findInFile("file3.txt", pattern);
+        List<Integer> excepted = new ArrayList<>();
+        excepted.add(100000);
+        assertEquals(excepted, resultBoyerMoore);
+        newFile.delete();
+    }
+
+    @Test
+    void testReallyyBig() throws IOException {
+        int maxSize = 100000;
+        File newFile = new File("file4.txt");
+        OutputStreamWriter fileWriter = new OutputStreamWriter(new FileOutputStream(newFile), StandardCharsets.UTF_8);
+
+        char[] chunk = new char[maxSize];
+        Arrays.fill(chunk, 'h');
+
+        for (int i = 0; i < 10000; i++) {
+            fileWriter.write(chunk);
+        }
+
+        fileWriter.write("hello");
+
+        Arrays.fill(chunk, 'l');
+        for (int i = 0; i < 10000; i++) {
+            fileWriter.write(chunk);
+        }
+
+        fileWriter.close();
+
+        String pattern = "hello";
+        List<Integer> resultBoyerMoore = boyerMoore.findInFile("file4.txt", pattern);
+        List<Integer> excepted = new ArrayList<>();
+        excepted.add(1000000000);
+        assertEquals(excepted, resultBoyerMoore);
+        newFile.delete();
+    }
+    @Test
+    void testReallyyBigRussian() throws IOException {
+        int maxSize = 10000;
+        File newFile = new File("file5.txt");
+        OutputStreamWriter fileWriter = new OutputStreamWriter(new FileOutputStream(newFile), StandardCharsets.UTF_8);
+        char[] chunk = new char[maxSize];
+        String string = new String("й".getBytes(), StandardCharsets.UTF_8);
+
+        for (int i = 0; i < 1000000000; i++) {
+            fileWriter.write(string);
+        }
+        string = new String("прив".getBytes(), StandardCharsets.UTF_8);
+        fileWriter.write(string);
+
+        string = new String("л".getBytes(), StandardCharsets.UTF_8);
+        for (int i = 0; i < 1000000000; i++) {
+            fileWriter.write(string);
+        }
+        fileWriter.flush();
+        fileWriter.close();
+
+        String pattern = "прив";
+        List<Integer> resultBoyerMoore = boyerMoore.findInFile("file5.txt", pattern);
+        List<Integer> excepted = new ArrayList<>();
+        excepted.add(1000000000);
+        assertEquals(excepted, resultBoyerMoore);
+        newFile.delete();
+    }
+}
