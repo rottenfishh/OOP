da08a1003fd033c449f9db5294685d7f3dca4a8c
alina <alinakolodina591@gmail.com>
2025-04-24 21:01:16 +0700

    one more and i give up

--- Task_2_4_1/src/test/java/ru/nsu/kolodina/ooptasks/TestDSL.java
+++ Task_2_4_1/src/test/java/ru/nsu/kolodina/ooptasks/TestDSL.java
@@ -6,6 +6,8 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 import java.util.List;
 public class TestDSL {
     DriverTool driverTool;
@@ -37,6 +39,7 @@ public class TestDSL {
         driverTool.extractData(path);
         CheckPoint firstCheck = driverTool.getCheckPoint("midterm");
         Assignment assignment = driverTool.getAssignment("Alina");
+        repository = driverTool.getStudentRepo(assignment);
         Group.Student student = assignment.student;
         driverTool.runBuildChecks(student.buildTool,  repository, assignment.tasks.get(0));
     }
@@ -50,6 +53,37 @@ public class TestDSL {
         assertEquals(3, student.getMark());
     }
 
+    @Test
+    public void testClassLoading() {
+        driverTool.extractData(path);
+        Criteries criteriaCheck = null;
+        try {
+            criteriaCheck = Utils.loadClassInstance(driverTool.pathToClasses.get("criteries"), Criteries.class);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        Grading gradingCheck = null;
+        try {
+            gradingCheck = Utils.loadClassInstance(driverTool.pathToClasses.get("grading"), Grading.class);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        assertNotNull(criteriaCheck);
+        assertNotNull(gradingCheck);
+    }
+    @Test
+    public void testGit() {
+        driverTool.extractData(path);
+        CheckPoint firstCheck = driverTool.getCheckPoint("midterm");
+        Assignment alinaAssignment = driverTool.getAssignment("Alina");
+        repository = driverTool.getStudentRepo(alinaAssignment);
+        Git git = new Git();
+        List<String> res = new ArrayList<>();
+        git.getLastCommitDate(repository, alinaAssignment.tasksNames.get(0), res);
+        assertEquals(1, res.size());
+        git.getFirstCommitDate(repository, alinaAssignment.tasksNames.get(0), res);
+        ///assertEquals(2, res.size());
+    }
     @Test
     public void genHTML() {
         List<Assignment> assignments = driverTool.getAssignmentList();
