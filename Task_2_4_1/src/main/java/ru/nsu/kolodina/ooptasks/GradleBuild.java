217c3b10c4dbf9071428767a7bcedce737f5642b
alina <alinakolodina591@gmail.com>
2025-04-28 13:38:25 +0700

    aeeggh

--- Task_2_4_1/src/main/java/ru/nsu/kolodina/ooptasks/GradleBuild.java
+++ Task_2_4_1/src/main/java/ru/nsu/kolodina/ooptasks/GradleBuild.java
@@ -20,10 +20,10 @@ public class GradleBuild implements Build{
     public int compile(String repo, String task) {
         String dir = repo + File.separator + task;
         List<String> args = null;
-        //tr -d '\r' < filename > temp && mv temp filename
+        //cat filename | tr -d '\r' > temp && mv temp filename
         if (gradleName.equals("./gradlew")) {
             System.out.println("aaaaaaaaaaaa");
-            args = cmd.buildArgs("tr", "-d", "'\r", "<", gradleName, ">", "temp", "&&", "mv", "temp", gradleName);
+            args = cmd.buildArgs("sh", "-c", "cat", gradleName, "|", "tr", "-d", "'\r'", ">", "temp", "&&", "mv", "temp", gradleName);
             //args = cmd.buildArgs("dos2unix", gradleName);
             cmd.runCommand(dir, args, null, true);
         }
