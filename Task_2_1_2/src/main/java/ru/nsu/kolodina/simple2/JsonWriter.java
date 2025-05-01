68d5bc375110a3ef1bc9fe1817c303c988289d2f
alina <alinakolodina591@gmail.com>
2025-05-01 15:13:15 +0700

    ghhhhhhhhhhhhhhh

--- Task_2_1_2/src/main/java/ru/nsu/kolodina/simple2/JsonWriter.java
+++ Task_2_1_2/src/main/java/ru/nsu/kolodina/simple2/JsonWriter.java
@@ -2,10 +2,7 @@ package ru.nsu.kolodina.simple2;
 
 import org.json.JSONArray;
 
-import java.io.FileWriter;
-
 public class JsonWriter {
-
     public static JSONArray createJson(int[] arr) {
         JSONArray ja = new JSONArray();
         for (int i = 0; i < arr.length; i++) {
